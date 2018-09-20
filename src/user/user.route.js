22a8ed9d6eb0661f81189b1b16ddfefaff0b052a
adautofbn <barros.adauto@gmail.com>
2018-09-20 09:31:50 -0300

    user with mongo ok

--- src/user/user.route.js
+++ src/user/user.route.js
@@ -1,14 +1,8 @@
 const express = require('express');
 const router = new express.Router();
-const cache = require('memory-cache');
-
-const userUtil = require('./user.util');
 const validUtil = require('../util/validate.util');
 
-const users = require('./users.json');
 const UserModel = require('./user.model');
-
-cache.put('users', users);
 
 router.get('/', (req,res) => {
   UserModel.find({}).then((usersDb,err) => {
@@ -33,55 +27,44 @@ router.get('/:id', (req,res) => {
 });
 
 router.post('/', (req,res) => {
+  const {error} = validUtil.validateUser(req.body);
 
-    let message = '';
+  if (error) {
+    return res.status(400).json(error.details[0].message);
+  }
 
-    const {error} = validUtil.validateUser(req.body);
+  const userCollec = UserModel.estimatedDocumentCount();
 
-    if (error) {
-      return res.status(400).json(error.details[0].message);
-    }
+  userCollec.then((count) => {
+    const user = {
+      'id': count + 1,
+      'name': req.body.name,
+      'email': req.body.email.toLowerCase(),
+      'password': req.body.password,
+      'type': req.body.type.toLowerCase()
+    };
 
-    if (!message) {
-      const user = {
-        'id': users.length + 1,
-        'name': req.body.name,
-        'email': req.body.email.toLowerCase(),
-        'password': req.body.password,
-        'type': req.body.type.toLowerCase()
-      };
+    const newUser = new UserModel(user);
 
-      users.push(user);
-
-      const newUser = new UserModel(user);
-
-      newUser.save((err) => {
-        if (err) {
-          console.error(err);
-        }
-      });
-
-      message = `Usuário cadastrado com sucesso: ${user.name}`;
-    }
-    cache.put('users', users);
-    return res.status(201).json(message);
+    newUser.save((err) => {
+      if (err) {
+        console.error(err);
+      }
+    });
+  });
+  return res.status(201).json(`Usuário cadastrado com sucesso: ${req.body.name}`);
 });
 
 router.put('/:id', (req,res) => {
-    const user = userUtil.findUser(cache.get('users'),req.params.id);
-    if (!user) {
-      return res.status(404).json(`Usuário ${req.params.id} não encontrado`);
-    }
-
     const {error} = validUtil.validateUser(req.body, true);
 
     if (error) {
-      return res.status(400).json(error.details[0].message);
+      res.status(400).json(error.details[0].message);
     }
 
     UserModel.findOne({'id': req.params.id}).then((userDb,err) => {
       if (userDb === null || err) {
-        res.status(404).json(`Usuário ${req.params.id} não encontrado`);
+        return res.status(404).json(`Usuário ${req.params.id} não encontrado`);
       }
       userDb.name = req.body.name || userDb.name;
       userDb.email = req.body.email || userDb.email;
@@ -93,35 +76,17 @@ router.put('/:id', (req,res) => {
           console.log(errSave);
         }
       });
+      return res.status(200).json('Usuário atualizado com sucesso');
     });
-
-    user.name = req.body.name || user.name;
-    user.email = req.body.email || user.email;
-    user.password = req.body.password || user.password;
-    user.type = req.body.type || user.type;
-
-    cache.put('users', users);
-    return res.status(200).json(`Usuário atualizado com sucesso: ${user.name}`);
 });
 
 router.delete('/:id', (req,res) => {
-
-    const user = userUtil.findUser(cache.get('users'),req.params.id);
-    if (!user) {
-      return res.status(404).json(`Usuário ${req.params.id} não encontrado`);
-    }
-
     UserModel.deleteOne({'id': req.params.id}).then((err) => {
       if (err) {
         console.log(err);
       }
     });
-
-    const index = users.indexOf(user);
-    users.splice(index,1);
-
-    cache.put('users', users);
-    return res.status(200).json(`Usuário deletado com sucesso: ${user.name}`);
+    return res.status(200).json('Usuário deletado com sucesso');
 });
 
 module.exports = router;
