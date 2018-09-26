36552127b41563608f3f1a644cd77b056a2c8db1
adautofbn <barros.adauto@gmail.com>
2018-09-26 10:45:00 -0300

    bag with mongo

--- src/bag/bag.route.js
+++ src/bag/bag.route.js
@@ -2,8 +2,7 @@ const express = require('express');
 const router = new express.Router();
 const cache = require('memory-cache');
 
-const productUtil = require('../product/product.util');
-const validUtil = require('../util/validate.util');
+const ProductModel = require('../product/product.model');
 
 const bag = require('./bag.json');
 
@@ -22,59 +21,44 @@ router.get('/', (req, res) => {
 });
 
 router.post('/', (req,res) => {
-    let message = '';
-
-    const {error} = validUtil.validateId(req.body);
-
-    if (error) {
-      return res.status(400).json(error.details[0].message);
-    }
-
-    const product = productUtil.findProduct(cache.get('products'),req.body.id);
-    if (!product) {
-      message = `Item ${req.body.id} não encontrado`;
-      return res.status(404).json(message);
-    }
-
-    message = validUtil.validateQuant(product.quant, req.body.quant);
-    if (!message) {
-      product.quant = req.body.quant || 1;
-    }
-
-    bag.forEach((item) => {
-      if (item.id === product.id) {
-        const newQuant = item.quant + (product.quant || 1);
-
-        message = validUtil.validateQuant(product.quant, newQuant);
-        if (!message) {
-          item.quant = newQuant;
-
-          message = `Item ${item.name} já existe na bolsa, quantidade aumentada para ${item.quant}`;
-        }
+    ProductModel.findOne({'id': req.body.id}).then((product,err) => {
+      if (product === null || err) {
+        return res.status(400).json(`Item ${req.body.id} não encontrado`);
       }
-    });
 
-    if (!message) {
+      const reqQuant = req.body.quant || 1;
+      if (reqQuant > product.quant) {
+        return res.status(400).json(`Quantidade de ${product.name} indisponível`);
+      }
+
+      const bagProduct = bag.find((bagItem) => bagItem.id === product.id);
+      if (bagProduct) {
+        const bagNewQuant = reqQuant + bagProduct.quant;
+        if (bagNewQuant > product.quant) {
+          return res.status(400).json(`Quantidade de ${bagProduct.name} indisponível`);
+        }
+        bagProduct.quant = bagNewQuant;
+        cache.put('bag',bag);
+        return res.status(200).json(`Produto ${bagProduct.name} já existe na bolsa, quantidade incrementada`);
+      }
+
+      product.quant = reqQuant;
+
       bag.push(product);
-      message = `Item ${product.name} adicionado à sua bolsa`;
-    }
-
-    cache.put('bag',bag);
-    return res.status(200).json(message);
+      cache.put('bag',bag);
+      return res.status(200).json(`Produto adicionado à sua bolsa: ${product.name}`);
+    });
 });
 
 router.delete('/', (req,res) => {
-
-  const product = productUtil.findProduct(bag,req.query.id);
+  const product = bag.find((item) => item.id === parseInt(req.body.id));
   if (!product) {
-    return res.status(404).json(`Item ${req.query.id} não encontrado`);
+    return res.status(400).json(`Item ${req.body.id} não encontrado`);
   }
-
   const index = bag.indexOf(product);
   bag.splice(index,1);
-
-  cache.put('bag', bag);
-  return res.status(200).json(`Item deletado da sua bolsa com sucesso ${product.name}`);
+  cache.put('bag',bag);
+  return res.status(200).json(`Item deletado com sucesso: ${product.name}`);
 });
 
 module.exports = router;
